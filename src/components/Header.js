5bc6b08ce4cb700fb427b4ad028ef7b4ad525585
Mav <maverickcpaz@gmail.com>
2021-09-06 12:32:49 +0800

    Added links to button on Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,13 @@
 
 const Header = () => {
+    const gitHub = () => {
+        window.open("https://github.com/thisismav99");
+    }
+
+    const linkedIn = () => {
+        window.open("https://linkedin.com/in/mave-rick-paz-679250220");
+    } 
+
     return (
         <div className="container-fluid header">
             <div className="text-center">
@@ -8,9 +16,25 @@ const Header = () => {
                 <p className="position-container">WEB DEVELOPER</p>
 
                 <div className="mt-3">
-                    <button className="btn btn-light me-2"><i className="bi bi-envelope-fill email-btn"></i></button>
-                    <button className="btn btn-light me-2"><i className="bi bi-github github-btn"></i></button>
-                    <button className="btn btn-light"><i className="bi bi-linkedin linkedin-btn"></i></button>
+                    <button className="btn btn-light me-2" data-bs-toggle="collapse" data-bs-target="#collapseEmail"><i className="bi bi-envelope-fill email-btn"></i></button>
+                    <button onClick={() => gitHub()} className="btn btn-light me-2"><i className="bi bi-github github-btn"></i></button>
+                    <button onClick={() => linkedIn()} className="btn btn-light"><i className="bi bi-linkedin linkedin-btn"></i></button>
+                </div>
+
+                <div className="row">
+                    <div className="col-xl-3">
+
+                    </div>
+                    <div className="col-xl-6">
+                        <div className="collapse mt-3" id="collapseEmail">
+                            <div className="card card-body text-dark">
+                                maverickcpaz@gmail.com
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-xl-3">
+
+                    </div>
                 </div>
             </div>
         </div>
