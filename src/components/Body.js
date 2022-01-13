fe206b4ca4b778be8009f765fc0033031a42d7c4
Mav <maverickcpaz@gmail.com>
2022-01-13 14:38:22 +0800

    Change the date for CP2

--- src/components/Body.js
+++ src/components/Body.js
@@ -77,7 +77,7 @@ const Body = () => {
                                 <div className="accordion-item">
                                     <h2 className="accordion-header" id="flush-headingTwo">
                                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                                        COMPUTER PROGRAMMER II (PRESENT)
+                                        COMPUTER PROGRAMMER II (MARCH 2021 - DECEMBER 2021)
                                     </button>
                                     </h2>
                                     <div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
