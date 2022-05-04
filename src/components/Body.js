2b009e767c5a53945a58c8759f91c45644290847
Mav <maverickcpaz@gmail.com>
2022-05-04 11:58:26 +0800

    Update Body Component Data

--- src/components/Body.js
+++ src/components/Body.js
@@ -38,7 +38,7 @@ const Body = () => {
                                         <i className="bi bi-star-fill text-warning"></i>
                                         <i className="bi bi-star-half text-warning"></i>
                                     </p>
-                                    <span>C#, HTML, CSS, JavaScript</span>
+                                    <span>C#, Stored Procedures, SQL Database Functions, HTML, CSS, JavaScript</span>
                                 </li>
                                 <li className="list-group-item">
                                     <p>
@@ -49,6 +49,16 @@ const Body = () => {
                                         <i className="bi bi-star text-warning"></i>
                                     </p>
                                     <span>MS SQL, MySQL, SQL Server, Firebase</span>
+                                </li>
+                                <li className="list-group-item">
+                                    <p>
+                                        <i className="bi bi-star-fill text-warning"></i>
+                                        <i className="bi bi-star-fill text-warning"></i>
+                                        <i className="bi bi-star-fill text-warning"></i>
+                                        <i className="bi bi-star-fill text-warning"></i>
+                                        <i className="bi bi-star text-warning"></i>
+                                    </p>
+                                    <span>Github, Azure Dev Ops</span>
                                 </li>
                             </ul>
                         </div>
@@ -64,7 +74,7 @@ const Body = () => {
                                 <div className="accordion-item">
                                     <h2 className="accordion-header" id="flush-headingOne">
                                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                                        COMPUTER PROGRAMMER I (2019 - 2020)
+                                        COMPUTER PROGRAMMER I (JUL 2019 - DEC 2020)
                                     </button>
                                     </h2>
                                     <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
@@ -77,13 +87,25 @@ const Body = () => {
                                 <div className="accordion-item">
                                     <h2 className="accordion-header" id="flush-headingTwo">
                                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                                        COMPUTER PROGRAMMER II (MARCH 2021 - DECEMBER 2021)
+                                        COMPUTER PROGRAMMER II (MAR 2021 - DEC 2021)
                                     </button>
                                     </h2>
                                     <div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
                                         <div className="accordion-body text-dark">
                                             Planning, development, monitoring, maintenance and handling of overall process
                                             of IT Systems.
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="accordion-item">
+                                    <h2 className="accordion-header" id="flush-headingThree">
+                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                                        SOFTWARE ENGINEER (FEB 2022 - PRESENT)
+                                    </button>
+                                    </h2>
+                                    <div id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headinThree" data-bs-parent="#accordionFlushExample">
+                                        <div className="accordion-body text-dark">
+                                            Maintenance, support and developing/optimization of new functions for existing Payroll System and Database.
                                         </div>
                                     </div>
                                 </div>
@@ -99,7 +121,7 @@ const Body = () => {
                         <div className="card-body">
                             <p className="bg-light text-dark p-1 fst-italic about-container">
                                 <span className="quoute">"</span>
-                                    I've been developing websites for 2 years and always loving it! Fascinated by technology 
+                                    I've been developing websites for 3 years and always loving it! Fascinated by technology 
                                     advancement is the reason why I take this path. Let me be a part of your team and rock this world.
                                 <span className="quoute">"</span>
                             </p>
